e2aac89e91151c43b9697472affaca0d64239c92
richiexuetang <richard772794659@gmail.com>
2022-08-23 22:19:42 -0700

    update pdf pt2

--- gatsby-config.js
+++ gatsby-config.js
@@ -148,11 +148,5 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-45666519-2',
-      },
-    },
   ],
 };
